0fd61d92516980b69ee30b3ef003ab43686025c8
Sanjit Hyanju <sanjithyanju99@gmail.com>
2025-12-29 12:37:20 +0545

    chore: update Vite config and add path aliases

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import tailwindcss from "@tailwindcss/vite";
+import path from "node:path";
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -9,7 +10,14 @@ export default defineConfig({
         proxy: {
             "/api": {
                 target: "http://localhost:8080",
+                changeOrigin: true,
+                secure: false,
             },
+        },
+    },
+    resolve: {
+        alias: {
+            "@": path.resolve(__dirname, "./src"),
         },
     },
 });
